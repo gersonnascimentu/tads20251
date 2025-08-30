cd600bfdc71bb4ac3f7a4abdea9c84c69c858c22
Gerson Nascimentu <gerson.lima.nb@gmail.com>
2025-08-30 15:35:16 +0000

    sim

--- tabela_grafico.py
+++ tabela_grafico.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 tabela = pd.DataFrame({
     'idade': [20, 23, 45, 31, 32],
-    'nome' : ['joao', 'maria', 'jose', 'sofia', 'helena']
+    'nome': ['joao', 'maria', 'jose', 'sofia', 'helena']
 })
 
 plt.hist(tabela['idade'])
